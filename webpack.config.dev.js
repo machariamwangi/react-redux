2bd5a53ebda9ae5823b944e848afb45a910da187
Elimax Mwangi <elimax@localhost.localdomain>
2019-11-19 10:20:23 -0500

    configuration of babel, webpack and eslient

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -31,6 +31,7 @@ module.exports = {
   module: {
     rules: [
       {
+        // test: /\.(js|jsx)$/,
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader", "eslint-loader"]
